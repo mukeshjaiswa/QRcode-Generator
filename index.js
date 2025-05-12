7e7a67c3ac9b5c1a8166ff0cf12be2f5db73d7ba
Mukesh Kumar Chaudhary <1736mukesh@gmail.com>
2025-05-12 22:55:53 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+let text = document.querySelector(".input");
+let btn = document.querySelector("button")
+let img = document.querySelector("img")
+
+btn.addEventListener('click', () => {
+    if (text.value === '') {
+        alert("Please enter text or url to generater qr")
+    }
+    else{
+        img.style.display="block"
+        const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(text.value)}&size=200x200`;
+img.src=qrUrl;
+text.value=''
+    }
+})
